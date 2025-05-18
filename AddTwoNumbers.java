2c515451820babb94c09cdd781308a2e632fd7b9
nishanthydv <152245334+nishanthydv@users.noreply.github.com>
2025-05-19 00:45:10 +0530

    changed from add to sub

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,8 +1,9 @@
+//changed add method to sub
 public class AddTwoNumbers {
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 20;
-        int sum = num1 + num2;
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        int sum = num1 - num2;
+        System.out.println("The diff betw  " + num1 + " and " + num2 + " is: " + sum);
     }
 }
